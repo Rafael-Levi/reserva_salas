0549e5505194de1359ee4698fd27ac522ca6983d
Rafael <rafaellevicodur2@gmail.com>
2024-12-05 11:54:17 -0300

    feat: add conteúdo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BigCardComponent,NavBarComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
