09f37b835f46d74d5e2c984cb6e428a5b2744124
Rafael <rafaellevi@gmail.com>
2024-12-07 23:26:58 -0300

    feat: adicionando paginas e melhorando layout

--- pages/RH/RH.js
+++ pages/RH/RH.js
@@ -38,42 +38,15 @@ document.addEventListener("DOMContentLoaded", () => {
     generateWeek();
   });
 
-const formAgendamento = document.getElementById('form-agendamento');
-formAgendamento.addEventListener('submit', (event) => {
-    event.preventDefault(); // Previne o envio padrão do formulário
+daysElement.innerHTML = daysHTML;
 
-    // Lógica para processar o agendamento (ex: enviar dados para o servidor)
-
-    alert('Agendamento realizado com sucesso!');
-    formAgendamento.reset(); // Limpa o formulário
-});
-
+  
 function openPopup() {
     document.getElementById("popup").style.display = "flex";
     document.body.style.overflow = "hidden"; 
 }
-
+  
 function closePopup() {
     document.getElementById("popup").style.display = "none";
     document.body.style.overflow = "auto"; 
-}
-
-function formatarDataAtual() {
-    // Cria um novo objeto de data com a data e hora atuais
-    const dataAtual = new Date();
-
-    // Array com os nomes dos meses em português
-    const meses = [
-        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
-        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
-    ];
-
-    // Obtém o nome do mês, o dia e o ano
-    const nomeMes = meses[dataAtual.getMonth()];
-    const dia = dataAtual.getDate();
-    const ano = dataAtual.getFullYear();
-
-    // Formata a data no modelo desejado
-    return `${nomeMes} ${dia.toString().padStart(2, '0')}, ${ano}`;
-             
 }
---
+++ pages/home/script.js
@@ -0,0 +1,115 @@
+document.addEventListener("DOMContentLoaded", () => {
+  generateWeek();
+
+  const rhButton = document.getElementById("rh-button");
+  const popup = document.getElementById("popup-rh");
+  const closeButton = document.getElementById("close-popup-rh");
+  const form = document.getElementById("registration-form-rh");
+ 
+  rhButton.addEventListener("click", () => {
+    popup.style.display = "flex";
+    popup.body.style.overflow = "hidden";
+  });
+ 
+  closeButton.addEventListener("click", () => {
+    popup.style.display = "none";
+    popup.body.style.overflow = "auto"; 
+  });
+ 
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+ 
+    const mat = document.getElementById("mat-rh").value.trim();
+    const password = document.getElementById("senha-rh").value.trim();
+ 
+    if (mat == 'rafael' && password == '1234') {
+      alert("Cadastro validado com sucesso!");
+      window.location.href = "../RH/RH.html"; // Redireciona para outra página
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
+  });
+});
+  
+function generateWeek() {
+  const daysElement = document.getElementById("days");
+  const today = new Date();
+  const currentDayIndex = today.getDay();
+  const startOfWeek = new Date(today);
+  startOfWeek.setDate(today.getDate() - currentDayIndex + (currentDayIndex === 0 ? -6 : 1));
+
+  const weekDays = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"];
+  let daysHTML = "";
+
+  for (let i = 0; i < 7; i++) {
+    const currentDate = new Date(startOfWeek);
+    currentDate.setDate(startOfWeek.getDate() + i);
+
+    const dayName = weekDays[currentDate.getDay()];
+    const dayNumber = currentDate.getDate();
+    const monthNumber = currentDate.getMonth() + 1;
+
+    daysHTML += `
+      <div>
+        <span class="day">${dayName}</span>
+        <span class="date">${dayNumber.toString().padStart(2, "0")}/${monthNumber.toString().padStart(2, "0")}</span>
+      </div>
+    `;
+  }
+
+  daysElement.innerHTML = daysHTML;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cfgButton = document.getElementById("cfg-button");
+  const cfgClose = document.getElementById("close-popup-cfg");
+  const cfgPopup = document.getElementById("popup-cfg");
+  const submit = document.getElementById("registration")
+ 
+  // Abre o pop-up
+  cfgButton.addEventListener("click", () => {
+    cfgPopup.style.display = "flex";
+    cfgPopup.body.style.overflow = "hidden";
+  });
+
+  cfgClose.addEventListener("click", () => {
+    cfgPopup.style.display = "none";
+    cfgPopup.body.style.overflow = "auto"; 
+  });
+
+  // Valida o formulário e redireciona
+    submit.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const mat = document.getElementById("mat-cfg").value.trim();
+    const senha = document.getElementById("senha-cfg").value.trim();
+
+    if (mat == 'rafael' && senha == '1234') {
+      window.alert("Cadastro validado com sucesso!");
+      window.location.href = "../configurador/config.html";
+    } else {
+      alert("Por favor, preencha todos os campos.");
+    }
+  });
+});
+
+const formAgendamento = document.getElementById('form-agendamento');
+formAgendamento.addEventListener('submit', (event) => {
+    event.preventDefault(); // Previne o envio padrão do formulário
+
+    // Lógica para processar o agendamento (ex: enviar dados para o servidor)
+
+    alert('Agendamento realizado com sucesso!');
+    formAgendamento.reset(); // Limpa o formulário
+});
+
+function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+    document.body.style.overflow = "hidden"; 
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.body.style.overflow = "auto"; 
+}
